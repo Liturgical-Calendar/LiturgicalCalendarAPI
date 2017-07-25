61109c14c7ccd01bfffe12e69d3c6fec85d2f27f
John R. D'Orazio <john.dorazio@cappellaniauniroma3.org>
2017-07-26 01:11:27 +0200

    Create liturgy_config.php

---
+++ liturgy_config.php
@@ -0,0 +1,19 @@
+<?php
+    /* PLEASE DON'T REMOVE THIS FOR YOUR OWN SECURITY */
+    if(!defined("LITURGYAPP") || LITURGYAPP != "AMDG"){
+    	exit(0);
+    }
+    /* PLEASE DON'T REMOVE THIS FOR YOUR OWN SECURITY */
+
+
+    //BEGIN USER DEFINABLE DATABASE CONFIGURATION FOR LITURGY APPLICATION
+    define("DB_USER","");
+    define("DB_PASSWORD","");
+    define("DB_NAME","");
+    
+    //DON'T CHANGE THESE IF YOU DON'T HAVE TO:
+    ***REMOVED***
+    ***REMOVED***
+    
+    
+?>
