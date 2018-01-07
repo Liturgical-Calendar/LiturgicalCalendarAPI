c2a89172ac94e06d0b9c36adb6cbad9f39e73260
John R. D'Orazio <john.dorazio@cappellaniauniroma3.org>
2018-01-07 23:21:51 +0100

    Update and rename liturgy_config.php to LitCalConfig.php

---
+++ LitCalConfig.php
@@ -0,0 +1,29 @@
+<?php
+    /* PLEASE DON'T REMOVE THIS FOR YOUR OWN SECURITY */
+    if(!defined("LITURGYAPP") || LITURGYAPP != "AMDG"){
+    	exit(0);
+    }
+    /* PLEASE DON'T REMOVE THIS FOR YOUR OWN SECURITY */
+
+
+    /* DATABASE CONFIGURATION FOR LITURGY APPLICATION */
+
+    //IT IS POSSIBLE TO USE A SPECIFIC DATABASE USERNAME AND PASSWORD 
+    //FOR LOCALHOST INSTALLATION AND FOR REMOTE HOST INSTALLATION
+    if($_SERVER["REMOTE_ADDR"]=="127.0.0.1"){ //LOCALHOST INSTALLATION
+        define("DB_USER","");
+        define("DB_PASSWORD","");
+        define("DB_NAME","liturgy");
+        ***REMOVED***
+        ***REMOVED***
+	}
+	else{ //REMOTE HOST INSTALLATION
+        define("DB_USER","");
+        define("DB_PASSWORD","");
+        define("DB_NAME","liturgy");
+        ***REMOVED***
+        ***REMOVED***
+	}
+    
+    
+?>
