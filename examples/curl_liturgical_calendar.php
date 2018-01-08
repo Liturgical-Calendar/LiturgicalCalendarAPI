78e404d731c176323db6f9d1538d2bd278212c16
John R. D'Orazio <john.dorazio@cappellaniauniroma3.org>
2018-01-08 15:06:22 +0100

    reorder and add http status code messages

--- examples/curl_liturgical_calendar.php
+++ examples/curl_liturgical_calendar.php
@@ -9,9 +9,12 @@
  */
 
     /**************************
-     * BEGIN DISPLAY LOGIC
+     * DEFINE USEFUL FUNCTIONS
+     * AND ARRAYS
      * 
      *************************/
+     
+     
     function countSameDayEvents($currentKeyIndex,$EventsArray,&$cc){
       $Keys = array_keys($EventsArray);
       $currentFestivity = $EventsArray[$Keys[$currentKeyIndex]];
@@ -24,6 +27,177 @@
       }
     }
     
+    /** 
+     *  CLASS FESTIVITY
+     *  SIMILAR TO THE CLASS USED IN THE LITCAL PHP ENGINE, 
+     *  EXCEPT THAT IT CONVERTS PHP TIMESTAMP TO DATETIME OBJECT 
+     *  AND DOES NOT IMPLEMENT JSONSERIALIZABLE OR COMPARATOR FUNCTION
+     **/
+    class Festivity {
+        public $name;
+        public $date;
+        public $color;
+        public $type;
+        public $grade;
+        public $common;
+    
+        function __construct($name,$date,$color,$type,$grade=0,$common='') 
+        {
+            $this->name = $name;
+            $this->date = DateTime::createFromFormat('U', $date);
+            $this->color = $color;
+            $this->type = $type;
+            $this->grade = $grade;
+            $this->common = $common;
+        }            
+    }
+
+
+    /**
+     * StatusCodes provides named constants for
+     * HTTP protocol status codes. Written for the
+     * Recess Framework (http://www.recessframework.com/)
+     *
+     * @author Kris Jordan
+     * @license MIT
+     * @package recess.http
+     */
+    class StatusCodes {
+    
+        // [Informational 1xx]
+        const HTTP_CONTINUE                        = 100;
+        const HTTP_SWITCHING_PROTOCOLS             = 101;
+    
+        // [Successful 2xx]
+        const HTTP_OK                              = 200;
+        const HTTP_CREATED                         = 201;
+        const HTTP_ACCEPTED                        = 202;
+        const HTTP_NONAUTHORITATIVE_INFORMATION    = 203;
+        const HTTP_NO_CONTENT                      = 204;
+        const HTTP_RESET_CONTENT                   = 205;
+        const HTTP_PARTIAL_CONTENT                 = 206;
+    
+        // [Redirection 3xx]
+        const HTTP_MULTIPLE_CHOICES                = 300;
+        const HTTP_MOVED_PERMANENTLY               = 301;
+        const HTTP_FOUND                           = 302;
+        const HTTP_SEE_OTHER                       = 303;
+        const HTTP_NOT_MODIFIED                    = 304;
+        const HTTP_USE_PROXY                       = 305;
+        const HTTP_UNUSED                          = 306;
+        const HTTP_TEMPORARY_REDIRECT              = 307;
+    
+        // [Client Error 4xx]
+        const errorCodesBeginAt                    = 400;
+        const HTTP_BAD_REQUEST                     = 400;
+        const HTTP_UNAUTHORIZED                    = 401;
+        const HTTP_PAYMENT_REQUIRED                = 402;
+        const HTTP_FORBIDDEN                       = 403;
+        const HTTP_NOT_FOUND                       = 404;
+        const HTTP_METHOD_NOT_ALLOWED              = 405;
+        const HTTP_NOT_ACCEPTABLE                  = 406;
+        const HTTP_PROXY_AUTHENTICATION_REQUIRED   = 407;
+        const HTTP_REQUEST_TIMEOUT                 = 408;
+        const HTTP_CONFLICT                        = 409;
+        const HTTP_GONE                            = 410;
+        const HTTP_LENGTH_REQUIRED                 = 411;
+        const HTTP_PRECONDITION_FAILED             = 412;
+        const HTTP_REQUEST_ENTITY_TOO_LARGE        = 413;
+        const HTTP_REQUEST_URI_TOO_LONG            = 414;
+        const HTTP_UNSUPPORTED_MEDIA_TYPE          = 415;
+        const HTTP_REQUESTED_RANGE_NOT_SATISFIABLE = 416;
+        const HTTP_EXPECTATION_FAILED              = 417;
+    
+        // [Server Error 5xx]
+        const HTTP_INTERNAL_SERVER_ERROR           = 500;
+        const HTTP_NOT_IMPLEMENTED                 = 501;
+        const HTTP_BAD_GATEWAY                     = 502;
+        const HTTP_SERVICE_UNAVAILABLE             = 503;
+        const HTTP_GATEWAY_TIMEOUT                 = 504;
+        const HTTP_VERSION_NOT_SUPPORTED           = 505;
+    
+        private static $messages = array(
+            // [Informational 1xx]
+            100 => '100 Continue',
+            101 => '101 Switching Protocols',
+    
+            // [Successful 2xx]
+            200 => '200 OK',
+            201 => '201 Created',
+            202 => '202 Accepted',
+            203 => '203 Non-Authoritative Information',
+            204 => '204 No Content',
+            205 => '205 Reset Content',
+            206 => '206 Partial Content',
+    
+            // [Redirection 3xx]
+            300 => '300 Multiple Choices',
+            301 => '301 Moved Permanently',
+            302 => '302 Found',
+            303 => '303 See Other',
+            304 => '304 Not Modified',
+            305 => '305 Use Proxy',
+            306 => '306 (Unused)',
+            307 => '307 Temporary Redirect',
+    
+            // [Client Error 4xx]
+            400 => '400 Bad Request',
+            401 => '401 Unauthorized',
+            402 => '402 Payment Required',
+            403 => '403 Forbidden',
+            404 => '404 Not Found',
+            405 => '405 Method Not Allowed',
+            406 => '406 Not Acceptable',
+            407 => '407 Proxy Authentication Required',
+            408 => '408 Request Timeout',
+            409 => '409 Conflict',
+            410 => '410 Gone',
+            411 => '411 Length Required',
+            412 => '412 Precondition Failed',
+            413 => '413 Request Entity Too Large',
+            414 => '414 Request-URI Too Long',
+            415 => '415 Unsupported Media Type',
+            416 => '416 Requested Range Not Satisfiable',
+            417 => '417 Expectation Failed',
+    
+            // [Server Error 5xx]
+            500 => '500 Internal Server Error',
+            501 => '501 Not Implemented',
+            502 => '502 Bad Gateway',
+            503 => '503 Service Unavailable',
+            504 => '504 Gateway Timeout',
+            505 => '505 HTTP Version Not Supported'
+        );
+    
+        public static function httpHeaderFor($code)
+        {
+            return 'HTTP/1.1 ' . self::$messages[$code];
+        }
+    
+    
+        public static function getMessageForCode($code)
+        {
+            return self::$messages[$code];
+        }
+    
+        public static function isError($code)
+        {
+            return is_numeric($code) && $code >= self::HTTP_BAD_REQUEST;
+        }
+    
+        public static function canHaveBody($code)
+        {
+            return
+                // True if not in 100s
+                ($code < self::HTTP_CONTINUE || $code >= self::HTTP_OK)
+                && // and not 204 NO CONTENT
+                $code != self::HTTP_NO_CONTENT
+                && // and not 304 NOT MODIFIED
+                $code != self::HTTP_NOT_MODIFIED;
+        }
+    }
+
+
     $YEAR = (isset($_GET["year"]) && is_numeric($_GET["year"]) && ctype_digit($_GET["year"]) && strlen($_GET["year"])===4) ? (int)$_GET["year"] : (int)date("Y");
     
     $EPIPHANY = (isset($_GET["epiphany"]) && ($_GET["epiphany"] === "JAN6" || $_GET["epiphany"] === "SUNDAY_JAN2_JAN8") ) ? $_GET["epiphany"] : "JAN6";
@@ -42,7 +216,13 @@
     //define(CORPUSCHRISTI,"THURSDAY");
     //define(CORPUSCHRISTI,"SUNDAY");
         
-    //  Initiate curl
+    $GRADE = array("","COMMEMORATION","OPTIONAL MEMORIAL","MEMORIAL","FEAST","FEAST OF THE LORD","SOLEMNITY","HOLY DAY OF OBLIGATION");
+    
+    $SUNDAY_CYCLE = array("A","B","C");
+    $WEEKDAY_CYCLE = array("I","II");
+    
+    
+    //  Initiate curl for communication with the LitCal server
     $ch = curl_init();
 
     $prefix = $_SERVER['HTTPS'] ? 'https://' : 'http://';
@@ -73,52 +253,12 @@
             // (not found, bad request, etc.) and 5xx (usually concerning
             // errors/exceptions in the remote script execution)
     
-            die("Request failed. HTTP status code: " . $resultStatus);
+            die("Request failed. HTTP status code: " . StatusCodes::getMessageForCode($resultStatus));
         }
     }
 
-    class Festivity {
-        public $name;
-        public $date;
-        public $color;
-        public $type;
-        public $grade; //1=Optional memorial,2=Obligatory memorial,3=Feast,4=Holy Day of Obligation,5=Feast of the Lord,6=Solemnity
-        public $common;
-    
-        function __construct($name,$date,$color,$type,$grade=0,$common='') 
-        {
-            $this->name = $name;
-            $this->date = DateTime::createFromFormat('U', $date);
-            $this->color = $color;
-            $this->type = $type;
-            $this->grade = $grade;
-            $this->common = $common;
-        }
-    
-        /* * * * * * * * * * * * * * * * * * * * * * * * *
-         * Funzione statica di comparazione
-         * in vista dell'ordinamento di un array di oggetti Festivity
-         * Tiene conto non soltanto del valore della data,
-         * ma anche del grado della festa qualora ci fosse una concomitanza
-         * * * * * * * * * * * * * * * * * * * * * * * * * */
-        public static function comp_date($a, $b) 
-        {
-            if ($a->date == $b->date) {
-                if($a->grade == $b->grade){
-                    return 0;
-                }
-                return ($a->grade > $b->grade) ? +1 : -1;
-            }
-            return ($a->date > $b->date) ? +1 : -1;
-        }
-        
-    }
 
-    $GRADE = array("","COMMEMORATION","OPTIONAL MEMORIAL","MEMORIAL","FEAST","FEAST OF THE LORD","SOLEMNITY","HOLY DAY OF OBLIGATION");
-    
-    $SUNDAY_CYCLE = array("A","B","C");
-    $WEEKDAY_CYCLE = array("I","II");
-    
+    // Gather the json results from the server into $LitCal array similar to the PHP Engine
     $LitCal = array();    
 
     $LitCalData = json_decode($result,true); // decode as associative array rather than stdClass object
@@ -132,24 +272,31 @@
     curl_close($ch);
     
     foreach($LitCal as $key => $value){
+        // retransform each entry from an associative array to a Festivity class object
         $LitCal[$key] = new Festivity($LitCal[$key]["name"],$LitCal[$key]["date"],$LitCal[$key]["color"],$LitCal[$key]["type"],$LitCal[$key]["grade"],$LitCal[$key]["common"]);
     }
+
+    /**************************
+     * BEGIN DISPLAY LOGIC
+     * 
+     *************************/
+    
 ?>
 <!doctype html>
 <head>
     <title>Generate Roman Calendar</title>
     <meta charset="UTF-8">
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="icon" type="image/x-icon" href="../favicon.ico">
     <meta name="msapplication-TileColor" content="#ffffff" />
-    <meta name="msapplication-TileImage" content="easter-egg-5-144-279148.png">
-    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="easter-egg-5-152-279148.png">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="easter-egg-5-144-279148.png">
-    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="easter-egg-5-120-279148.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="easter-egg-5-114-279148.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="easter-egg-5-72-279148.png">    
-    <link rel="apple-touch-icon-precomposed" href="easter-egg-5-57-279148.png">
-    <link rel="icon" href="/path/to/easter-egg-5-32-279148.png" sizes="32x32">
-    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> -->
+    <meta name="msapplication-TileImage" content="../easter-egg-5-144-279148.png">
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="../easter-egg-5-152-279148.png">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../easter-egg-5-144-279148.png">
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="../easter-egg-5-120-279148.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../easter-egg-5-114-279148.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../easter-egg-5-72-279148.png">    
+    <link rel="apple-touch-icon-precomposed" href="../easter-egg-5-57-279148.png">
+    <link rel="icon" href="../easter-egg-5-32-279148.png" sizes="32x32">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
 
