96d3971c3d186120b822b36f930431afbc3bc90f
John R. D'Orazio <john.dorazio@cappellaniauniroma3.org>
2018-01-09 09:48:45 +0100

    move calcGregEaster to LitCalFunctions.php

--- LitCalEngine.php
+++ LitCalEngine.php
@@ -15,6 +15,8 @@ define("LITURGYAPP","AMDG"); //definition needed to allow inclusion of liturgy_c
 
 include "Festivity.php"; //this defines a "Festivity" class that can hold all the useful information about a single celebration
 
+include "LitCalFunctions.php"; //a few useful functions e.g. calculate Easter...
+
 include "LitCalConfig.php"; //this is where database connection info is defined
 $mysqli = new mysqli(DB_SERVER,DB_USER,DB_PASSWORD,DB_NAME);
 
@@ -70,32 +72,6 @@ else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     define("CORPUSCHRISTI",$CORPUSCHRISTI);
     //define(CORPUSCHRISTI,"THURSDAY");
     //define(CORPUSCHRISTI,"SUNDAY");
-
-
-
-    // https://en.wikipedia.org/wiki/Computus#Anonymous_Gregorian_algorithm
-    // aka Meeus/Jones/Butcher algorithm
-    
-    function calcGregEaster($Y){
-      $a = $Y % 19;
-      $b = floor($Y/100);
-      $c = $Y % 100;
-      $d = floor($b / 4);
-      $e = $b % 4;
-      $f = floor( ($b+8) / 25 );
-      $g = floor( ($b-$f+1) / 3 );
-      $h = (19*$a + $b - $d - $g + 15) % 30;
-      $i = floor($c/4);
-      $k = $c % 4;
-      $l = (32 + 2*$e + 2*$i - $h - $k) % 7;
-      $m = floor( ($a+11*$h+22*$l) / 451 );
-      $month = floor( ($h + $l - 7*$m + 114) / 31 );
-      $day = ( ($h + $l - 7*$m + 114) % 31) + 1;
-  
-      $dateObj   = DateTime::createFromFormat('!j-n-Y', $day.'-'.$month.'-'.$Y);
-      
-      return $dateObj;
-    }
 
 
     
