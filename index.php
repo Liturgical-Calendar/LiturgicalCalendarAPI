02c12af86631cd1c4decceaa16fee2f38dcb8dde
John D'Orazio <priest@johnromanodorazio.com>
2018-12-17 11:54:10 +0100

    Add links to example files from index.php
    With an index.php navigation to the examples is that much easier

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<head>
+    <title>Generate Roman Calendar</title>
+    <meta charset="UTF-8">
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+
+<?php
+
+    echo '<h1 style="text-align:center;">Liturgical Calendar - Universal or General Roman Calendar</h1>';
+    echo '<h2 style="text-align:center;"><a href="examples/javascript_liturgical_calendar.php">HTML presentation elaborated by JAVASCRIPT using an AJAX request</a></h2>';
+    echo '<h2 style="text-align:center;"><a href="examples/curl_liturgical_calendar.php">HTML presentation elaborated by PHP using a CURL request</a></h2>';
+
+?>
+</body>
