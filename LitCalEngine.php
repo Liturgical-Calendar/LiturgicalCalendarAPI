64990cf73164d6ef6fb814e1577cddf0f20324cf
John R. D'Orazio <priest@johnromanodorazio.com>
2018-12-18 15:07:47 +0100

    else if is not the same as elseif!

--- LitCalEngine.php
+++ LitCalEngine.php
@@ -614,7 +614,7 @@ else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
                         $LitCal["ImmaculateHeart"]->grade = MEMORIALOPT;
                         $LitCal[$row["TAG"]]->grade = MEMORIALOPT;
                     }
-		    else if(isset($LitCal["ImmaculateHeart"])){
+		    elseif(isset($LitCal["ImmaculateHeart"])){
 		        array_push($FEASTS_MEMORIALS,$LitCal["ImmaculateHeart"]->date);
 		    }
                 }
