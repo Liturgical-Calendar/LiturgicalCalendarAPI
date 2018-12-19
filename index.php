c01dde6b9e9739c292b94b3ca512800d662a943e
John R. D'Orazio <priest@johnromanodorazio.com>
2018-12-19 15:17:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
 <?php
 
     echo '<h1 style="text-align:center;">Liturgical Calendar Project</h1>';
-    echo '<h2 style="text-align:center;">Liturgical Calendar engine with algorithm to generate the Universal or General Roman Calendar for any given year</h2>';
-    echo '<h2 style="text-align:center;"><a href="LitCalEngine.php">(data generation endpoint here)</a></h2>';
+    echo '<h3 style="text-align:center;">Liturgical Calendar engine with algorithm to generate the Universal or General Roman Calendar for any given year</h2>';
+    echo '<h3 style="text-align:center;"><a href="LitCalEngine.php">(data generation endpoint here)</a></h2>';
     echo '<hr>';
-    echo '<h3 style="text-align:center;"><a href="examples/javascript_liturgical_calendar.php">HTML presentation elaborated by JAVASCRIPT using an AJAX request</a></h3>';
-    echo '<h3 style="text-align:center;"><a href="examples/curl_liturgical_calendar.php">HTML presentation elaborated by PHP using a CURL request</a></h3>';
+    echo '<h2 style="text-align:center;"><a href="examples/javascript_liturgical_calendar.php">HTML presentation elaborated by JAVASCRIPT using an AJAX request</a></h3>';
+    echo '<h2 style="text-align:center;"><a href="examples/curl_liturgical_calendar.php">HTML presentation elaborated by PHP using a CURL request</a></h3>';
 ?>
 </body>
