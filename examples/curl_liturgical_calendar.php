d443ecd05cac90a659894a9da4ac2e70eb867ea0
John R. D'Orazio <priest@johnromanodorazio.com>
2018-12-20 23:15:13 +0100

    add option to select locale

--- examples/curl_liturgical_calendar.php
+++ examples/curl_liturgical_calendar.php
@@ -226,6 +226,7 @@
     $EPIPHANY = (isset($_GET["epiphany"]) && ($_GET["epiphany"] === "JAN6" || $_GET["epiphany"] === "SUNDAY_JAN2_JAN8") ) ? $_GET["epiphany"] : "JAN6";
     $ASCENSION = (isset($_GET["ascension"]) && ($_GET["ascension"] === "THURSDAY" || $_GET["ascension"] === "SUNDAY") ) ? $_GET["ascension"] : "SUNDAY";
     $CORPUSCHRISTI = (isset($_GET["corpuschristi"]) && ($_GET["corpuschristi"] === "THURSDAY" || $_GET["corpuschristi"] === "SUNDAY") ) ? $_GET["corpuschristi"] : "SUNDAY";
+    $LOCALE = isset($_GET["locale"]) ? strtoupper($_GET["locale"]) ? "LA"; //default to latin
 
     define("EPIPHANY",$EPIPHANY);
     //define(EPIPHANY,"SUNDAY_JAN2_JAN8");
@@ -293,63 +294,63 @@
     $SUNDAY_CYCLE = array("A","B","C");
     $WEEKDAY_CYCLE = array("I","II");
     
-    
-    //  Initiate curl for communication with the LitCal server
-    $ch = curl_init();
+    if($YEAR >= 1970){
+	    //  Initiate curl for communication with the LitCal server
+	    $ch = curl_init();
 
-    $prefix = $_SERVER['HTTPS'] ? 'https://' : 'http://';
-    $domain = $_SERVER['HTTP_HOST'];
-    $query = $_SERVER['PHP_SELF'];
-    $path_info = pathinfo( $query );    
-    $URL =  $prefix . $domain . "/" . dirname($path_info['dirname']) . "/LitCalEngine.php";
+	    $prefix = $_SERVER['HTTPS'] ? 'https://' : 'http://';
+	    $domain = $_SERVER['HTTP_HOST'];
+	    $query = $_SERVER['PHP_SELF'];
+	    $path_info = pathinfo( $query );    
+	    $URL =  $prefix . $domain . "/" . dirname($path_info['dirname']) . "/LitCalEngine.php";
 
-    // Disable SSL verification
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    // Will return the response, if false it print the response
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    // Set the url
-    curl_setopt($ch, CURLOPT_URL, $URL);
-    // Set request method to POST
-    curl_setopt($ch, CURLOPT_POST, 1);
-    // Define the POST field data    
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array("year"=>$YEAR,"epiphany"=>$EPIPHANY,"ascension"=>$ASCENSION,"corpuschristi"=>$CORPUSCHRISTI)));    
-    // Execute
-    $result=curl_exec($ch);
-    
-    if (curl_errno($ch)) {
-        // this would be your first hint that something went wrong
-        die("Could not send request. Curl error: " . curl_error($ch));
-    } else {
-        // check the HTTP status code of the request
-        $resultStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        if ($resultStatus != 200) {
-            // the request did not complete as expected. common errors are 4xx
-            // (not found, bad request, etc.) and 5xx (usually concerning
-            // errors/exceptions in the remote script execution)
-    
-            die("Request failed. HTTP status code: " . StatusCodes::getMessageForCode($resultStatus));
-        }
+	    // Disable SSL verification
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	    // Will return the response, if false it print the response
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    // Set the url
+	    curl_setopt($ch, CURLOPT_URL, $URL);
+	    // Set request method to POST
+	    curl_setopt($ch, CURLOPT_POST, 1);
+	    // Define the POST field data    
+	    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array("year"=>$YEAR,"epiphany"=>$EPIPHANY,"ascension"=>$ASCENSION,"corpuschristi"=>$CORPUSCHRISTI,"locale"=>$LOCALE)));    
+	    // Execute
+	    $result=curl_exec($ch);
+
+	    if (curl_errno($ch)) {
+		// this would be your first hint that something went wrong
+		die("Could not send request. Curl error: " . curl_error($ch));
+	    } else {
+		// check the HTTP status code of the request
+		$resultStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		if ($resultStatus != 200) {
+		    // the request did not complete as expected. common errors are 4xx
+		    // (not found, bad request, etc.) and 5xx (usually concerning
+		    // errors/exceptions in the remote script execution)
+
+		    die("Request failed. HTTP status code: " . StatusCodes::getMessageForCode($resultStatus));
+		}
+	    }
+
+	    // Closing
+	    curl_close($ch);
+
+
+	    // Gather the json results from the server into $LitCal array similar to the PHP Engine
+	    $LitCal = array();    
+
+	    $LitCalData = json_decode($result,true); // decode as associative array rather than stdClass object
+	    if(isset($LitCalData["Settings"]) ){ $YEAR = $LitCalData["Settings"]["YEAR"]; }
+	    if(isset($LitCalData["LitCal"]) ){ $LitCal = $LitCalData["LitCal"]; }
+	    else{
+		die("We do not have enough information. Returned data has no LitCal property:" . var_dump($LitCalData) );
+	    }
+
+	    foreach($LitCal as $key => $value){
+		// retransform each entry from an associative array to a Festivity class object
+		$LitCal[$key] = new Festivity($LitCal[$key]["name"],$LitCal[$key]["date"],$LitCal[$key]["color"],$LitCal[$key]["type"],$LitCal[$key]["grade"],$LitCal[$key]["common"]);
+	    }
     }
-
-    // Closing
-    curl_close($ch);
-    
-
-    // Gather the json results from the server into $LitCal array similar to the PHP Engine
-    $LitCal = array();    
-
-    $LitCalData = json_decode($result,true); // decode as associative array rather than stdClass object
-    if(isset($LitCalData["Settings"]) ){ $YEAR = $LitCalData["Settings"]["YEAR"]; }
-    if(isset($LitCalData["LitCal"]) ){ $LitCal = $LitCalData["LitCal"]; }
-    else{
-        die("We do not have enough information. Returned data has no LitCal property:" . var_dump($LitCalData) );
-    }
-
-    foreach($LitCal as $key => $value){
-        // retransform each entry from an associative array to a Festivity class object
-        $LitCal[$key] = new Festivity($LitCal[$key]["name"],$LitCal[$key]["date"],$LitCal[$key]["color"],$LitCal[$key]["type"],$LitCal[$key]["grade"],$LitCal[$key]["common"]);
-    }
-
     /**************************
      * BEGIN DISPLAY LOGIC
      * 
@@ -371,9 +372,9 @@
     echo '<h1 style="text-align:center;">Liturgical Calendar Calculation for a Given Year ('.$YEAR.')</h1>';
     echo '<h2 style="text-align:center;">HTML presentation elaborated by PHP using a CURL request to a <a href="../LitCalEngine.php">PHP engine</a></h2>';
     
-    if($YEAR < 1969){
+    if($YEAR < 1970){
         echo '<div style="text-align:center;border:3px ridge Green;background-color:LightBlue;width:75%;margin:10px auto;padding:10px;">';
-        echo 'You are viewing a year prior to 1969: the calendar produced will not reflect the calendar used that year, but rather how the current Roman calendar would have been applied in that year.';
+        echo 'You are requesting a year prior to 1970: it is not possible to request years prior to 1970.';
         echo '</div>';        
     }
 
@@ -385,7 +386,9 @@
     echo '<td><label>EPIPHANY: <select name="epiphany" id="epiphany"><option value="JAN6" '.(EPIPHANY==="JAN6"?" SELECTED":"").'>January 6</option><option value="SUNDAY_JAN2_JAN8" '.(EPIPHANY==="SUNDAY_JAN2_JAN8"?" SELECTED":"").'>Sunday between January 2 and January 8</option></select></label></td>';
     echo '<td><label>ASCENSION: <select name="ascension" id="ascension"><option value="THURSDAY" '.(ASCENSION==="THURSDAY"?" SELECTED":"").'>Thursday</option><option value="SUNDAY" '.(ASCENSION==="SUNDAY"?" SELECTED":"").'>Sunday</option></select></label></td>';
     echo '<td><label>CORPUS CHRISTI (CORPUS DOMINI): <select name="corpuschristi" id="corpuschristi"><option value="THURSDAY" '.(CORPUSCHRISTI==="THURSDAY"?" SELECTED":"").'>Thursday</option><option value="SUNDAY" '.(CORPUSCHRISTI==="SUNDAY"?" SELECTED":"").'>Sunday</option></select></label></td>';
-    echo '<td><input type="SUBMIT" value="GENERATE CALENDAR" /></td>';
+    echo '<td><label>LOCALE: <select name="locale" id="locale"><option value="EN" '.($LOCALE==="EN"?" SELECTED":"").'>EN</option><option value="IT" '.($LOCALE==="IT"?" SELECTED":"").'>IT</option><option value="LA" '.($LOCALE==="LA"?" SELECTED":"").'>LA</option></select></label></td>';
+    echo '</tr><tr>';
+    echo '<td colspan="5"><input type="SUBMIT" value="GENERATE CALENDAR" /></td>';
     echo '</tr></table>';
     echo '</form>';
     echo '</fieldset>';
@@ -397,109 +400,110 @@
     
     echo '</div>';
     
-    echo '<table id="LitCalTable" style="width:75%;margin:30px auto;border:1px solid Blue;border-radius: 6px; padding:10px;background:LightBlue;">';
-    echo '<thead><tr><th>Date in Gregorian Calendar</th><th>General Roman Calendar Festivity</th><th>Grade of the Festivity</th></tr></thead>';
-    echo '<tbody>';
-    
-    
-    $dayCnt = 0;
-    //for($i=1997;$i<=2037;$i++){
-    $highContrast = array('purple','red','green');
-    
-    $LitCalKeys = array_keys($LitCal);
-    //print_r($LitCalKeys);
-    //echo count($LitCalKeys);
-    for($keyindex=0; $keyindex < count($LitCalKeys); $keyindex++){
-      $dayCnt++;
-      $keyname = $LitCalKeys[$keyindex];
-      $festivity = $LitCal[$keyname];
-      
-      //LET'S CALCULATE THE LITURGICAL YEAR CYCLE
-      $currentCycle = '';
-      //if we're dealing with a Sunday or a Solemnity or a Feast of the Lord, then we calculate the Sunday/Festive Cycle
-      if((int)$festivity->date->format('N') === 7 || (int)$festivity->grade > FEAST){
-        if($festivity->date < $LitCal["Advent1"]->date){ //$festivity->date >= DateTime::createFromFormat('!j-n-Y', '1-1-'.$YEAR) && 
-            $currentCycle = "YEAR ".($SUNDAY_CYCLE[($YEAR-1) % 3]);
-        }
-        else if($festivity->date >= $LitCal["Advent1"]->date){ // && $festivity->date <= DateTime::createFromFormat('!j-n-Y', '31-12-'.$YEAR)
-            $currentCycle = "YEAR ".($SUNDAY_CYCLE[$YEAR % 3]);
-        }
-      }
-      //otherwise we calculate the weekday cycle
-      else{
-        if($festivity->date < $LitCal["Advent1"]->date){ //$festivity->date >= DateTime::createFromFormat('!j-n-Y', '1-1-'.$YEAR) && 
-            $currentCycle = "YEAR ".($WEEKDAY_CYCLE[($YEAR-1) % 2]);
-        }
-        else if($festivity->date >= $LitCal["Advent1"]->date){ // && $festivity->date <= DateTime::createFromFormat('!j-n-Y', '31-12-'.$YEAR)
-            $currentCycle = "YEAR ".($WEEKDAY_CYCLE[$YEAR % 2]);
-        }      
-      }
-      
-      
-      //Let's check if we have more than one event on the same day, such as optional memorials...
-      $cc = 0; 
-      countSameDayEvents($keyindex,$LitCal,$cc);
-      if($cc>0){
-      
-        for($ev=0;$ev<=$cc;$ev++){
-            $keyname = $LitCalKeys[$keyindex];
-            $festivity = $LitCal[$keyname];
-            // LET'S DO SOME MORE MANIPULATION ON THE FESTIVITY->COMMON STRINGS AND THE FESTIVITY->COLOR...
-            if($festivity->common !== "" && $festivity->common !== "Proper"){
-              $commons = explode("|",$festivity->common);
-              $commons = array_map(function($txt){ $txt = str_replace(":",": ",$txt); return "from the Common of ".$txt; },$commons);
-              $festivity->common = implode("; or ",$commons);
-            }
-            $festivity->color = explode("|",$festivity->color)[0];
-           
-            //check which liturgical season we are in, to use the right color for that season...
-            $color = "green";
-            if(($festivity->date > $LitCal["Advent1"]->date  && $festivity->date < $LitCal["Christmas"]->date) || ($festivity->date > $LitCal["AshWednesday"]->date && $festivity->date < $LitCal["Easter"]->date )){
-                $color = "purple";
-            }
-            else if($festivity->date > $LitCal["Easter"]->date && $festivity->date < $LitCal["Pentecost"]->date){
-                $color = "white";
-            }
-            else if($festivity->date > $LitCal["Christmas"]->date || $festivity->date < $LitCal["BaptismLord"]->date ){
-                $color = "white";
-            }
-            
-            
-            echo '<tr style="background-color:'.$color.';'.(in_array($color,$highContrast)?'color:white;':'').'">';
-            if($ev==0){
-                $rwsp = $cc+1;
-                echo '<td rowspan="'.$rwsp.'" style="font-family:\'DejaVu Sans Mono\';font-size:.7em;font-weight:bold;">'.$festivity->date->format('D, F jS, Y').'</td>';
-            }
-            echo '<td>'.$festivity->name.' ('.$currentCycle.') - <i>'.$festivity->color.'</i><br /><i>'.$festivity->common.'</i></td>';
-            echo '<td>'.$GRADE[$festivity->grade].'</td>';
-            echo '</tr>';
-            $keyindex++;        
-        }
-        $keyindex--;
-      }
-      
-      else{
-        // LET'S DO SOME MORE MANIPULATION ON THE FESTIVITY->COMMON STRINGS AND THE FESTIVITY->COLOR...
-        if($festivity->common !== "" && $festivity->common !== "Proper"){
-          $commons = explode("|",$festivity->common);
-          $commons = array_map(function($txt){ $txt = str_replace(":",": ",$txt); return "From the Common of ".$txt; },$commons);
-          $festivity->common = implode("; or ",$commons);
-        }
-        $festivity->color = explode("|",$festivity->color)[0];
-        echo '<tr style="background-color:'.$festivity->color.';'.(in_array($festivity->color,$highContrast)?'color:white;':'').'">';
-        echo '<td style="font-family:\'DejaVu Sans Mono\';font-size:.7em;font-weight:bold;">'.$festivity->date->format('D, F jS, Y').'</td>';
-        echo '<td>'.$festivity->name.' ('.$currentCycle.') - <i>'.$festivity->color.'</i><br /><i>'.$festivity->common.'</i></td>';
-        echo '<td>'.$GRADE[$festivity->grade].'</td>';
-        echo '</tr>';      
-      }
-     
-      
-    }
-    
-    echo '</tbody></table>';
-    
-    echo '<div style="text-align:center;border:3px ridge Green;background-color:LightBlue;width:75%;margin:10px auto;padding:10px;">'.$dayCnt.' event days created</div>';
+    if($YEAR >= 1970){
+	    echo '<table id="LitCalTable" style="width:75%;margin:30px auto;border:1px solid Blue;border-radius: 6px; padding:10px;background:LightBlue;">';
+	    echo '<thead><tr><th>Date in Gregorian Calendar</th><th>General Roman Calendar Festivity</th><th>Grade of the Festivity</th></tr></thead>';
+	    echo '<tbody>';
 
+
+	    $dayCnt = 0;
+	    //for($i=1997;$i<=2037;$i++){
+	    $highContrast = array('purple','red','green');
+
+	    $LitCalKeys = array_keys($LitCal);
+	    //print_r($LitCalKeys);
+	    //echo count($LitCalKeys);
+	    for($keyindex=0; $keyindex < count($LitCalKeys); $keyindex++){
+	      $dayCnt++;
+	      $keyname = $LitCalKeys[$keyindex];
+	      $festivity = $LitCal[$keyname];
+
+	      //LET'S CALCULATE THE LITURGICAL YEAR CYCLE
+	      $currentCycle = '';
+	      //if we're dealing with a Sunday or a Solemnity or a Feast of the Lord, then we calculate the Sunday/Festive Cycle
+	      if((int)$festivity->date->format('N') === 7 || (int)$festivity->grade > FEAST){
+		if($festivity->date < $LitCal["Advent1"]->date){ //$festivity->date >= DateTime::createFromFormat('!j-n-Y', '1-1-'.$YEAR) && 
+		    $currentCycle = "YEAR ".($SUNDAY_CYCLE[($YEAR-1) % 3]);
+		}
+		else if($festivity->date >= $LitCal["Advent1"]->date){ // && $festivity->date <= DateTime::createFromFormat('!j-n-Y', '31-12-'.$YEAR)
+		    $currentCycle = "YEAR ".($SUNDAY_CYCLE[$YEAR % 3]);
+		}
+	      }
+	      //otherwise we calculate the weekday cycle
+	      else{
+		if($festivity->date < $LitCal["Advent1"]->date){ //$festivity->date >= DateTime::createFromFormat('!j-n-Y', '1-1-'.$YEAR) && 
+		    $currentCycle = "YEAR ".($WEEKDAY_CYCLE[($YEAR-1) % 2]);
+		}
+		else if($festivity->date >= $LitCal["Advent1"]->date){ // && $festivity->date <= DateTime::createFromFormat('!j-n-Y', '31-12-'.$YEAR)
+		    $currentCycle = "YEAR ".($WEEKDAY_CYCLE[$YEAR % 2]);
+		}      
+	      }
+
+
+	      //Let's check if we have more than one event on the same day, such as optional memorials...
+	      $cc = 0; 
+	      countSameDayEvents($keyindex,$LitCal,$cc);
+	      if($cc>0){
+
+		for($ev=0;$ev<=$cc;$ev++){
+		    $keyname = $LitCalKeys[$keyindex];
+		    $festivity = $LitCal[$keyname];
+		    // LET'S DO SOME MORE MANIPULATION ON THE FESTIVITY->COMMON STRINGS AND THE FESTIVITY->COLOR...
+		    if($festivity->common !== "" && $festivity->common !== "Proper"){
+		      $commons = explode("|",$festivity->common);
+		      $commons = array_map(function($txt){ $txt = str_replace(":",": ",$txt); return "from the Common of ".$txt; },$commons);
+		      $festivity->common = implode("; or ",$commons);
+		    }
+		    $festivity->color = explode("|",$festivity->color)[0];
+
+		    //check which liturgical season we are in, to use the right color for that season...
+		    $color = "green";
+		    if(($festivity->date > $LitCal["Advent1"]->date  && $festivity->date < $LitCal["Christmas"]->date) || ($festivity->date > $LitCal["AshWednesday"]->date && $festivity->date < $LitCal["Easter"]->date )){
+			$color = "purple";
+		    }
+		    else if($festivity->date > $LitCal["Easter"]->date && $festivity->date < $LitCal["Pentecost"]->date){
+			$color = "white";
+		    }
+		    else if($festivity->date > $LitCal["Christmas"]->date || $festivity->date < $LitCal["BaptismLord"]->date ){
+			$color = "white";
+		    }
+
+
+		    echo '<tr style="background-color:'.$color.';'.(in_array($color,$highContrast)?'color:white;':'').'">';
+		    if($ev==0){
+			$rwsp = $cc+1;
+			echo '<td rowspan="'.$rwsp.'" style="font-family:\'DejaVu Sans Mono\';font-size:.7em;font-weight:bold;">'.$festivity->date->format('D, F jS, Y').'</td>';
+		    }
+		    echo '<td>'.$festivity->name.' ('.$currentCycle.') - <i>'.$festivity->color.'</i><br /><i>'.$festivity->common.'</i></td>';
+		    echo '<td>'.$GRADE[$festivity->grade].'</td>';
+		    echo '</tr>';
+		    $keyindex++;        
+		}
+		$keyindex--;
+	      }
+
+	      else{
+		// LET'S DO SOME MORE MANIPULATION ON THE FESTIVITY->COMMON STRINGS AND THE FESTIVITY->COLOR...
+		if($festivity->common !== "" && $festivity->common !== "Proper"){
+		  $commons = explode("|",$festivity->common);
+		  $commons = array_map(function($txt){ $txt = str_replace(":",": ",$txt); return "From the Common of ".$txt; },$commons);
+		  $festivity->common = implode("; or ",$commons);
+		}
+		$festivity->color = explode("|",$festivity->color)[0];
+		echo '<tr style="background-color:'.$festivity->color.';'.(in_array($festivity->color,$highContrast)?'color:white;':'').'">';
+		echo '<td style="font-family:\'DejaVu Sans Mono\';font-size:.7em;font-weight:bold;">'.$festivity->date->format('D, F jS, Y').'</td>';
+		echo '<td>'.$festivity->name.' ('.$currentCycle.') - <i>'.$festivity->color.'</i><br /><i>'.$festivity->common.'</i></td>';
+		echo '<td>'.$GRADE[$festivity->grade].'</td>';
+		echo '</tr>';      
+	      }
+
+
+	    }
+
+	    echo '</tbody></table>';
+
+	    echo '<div style="text-align:center;border:3px ridge Green;background-color:LightBlue;width:75%;margin:10px auto;padding:10px;">'.$dayCnt.' event days created</div>';
+    }
     
 ?>
 </body>
