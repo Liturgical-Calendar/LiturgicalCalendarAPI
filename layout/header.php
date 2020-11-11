4ae758fe10f840bfbab31e7efe0529b3b98fc5e7
John R. D'Orazio <priest@johnromanodorazio.com>
2020-11-11 23:13:43 +0100

    use div.container-fluid instead of nav-bar
    simply because this allows us to have darker links! I couldn't hardly see the light gray links before

--- layout/header.php
+++ layout/header.php
@@ -6,11 +6,13 @@
 
         <!-- Sidebar - Brand -->
         <li><a class="sidebar-brand d-flex align-items-center justify-content-center" href="/LiturgicalCalendar">
-            <div class="sidebar-brand-text mx-3">Liturgical Calendar Project</div>
+                <div class="sidebar-brand-text mx-3">Liturgical Calendar Project</div>
             </a></li>
 
         <!-- Divider -->
-        <li><hr class="sidebar-divider my-0" /></li>
+        <li>
+            <hr class="sidebar-divider my-0" />
+        </li>
 
         <!-- Nav Item - Dashboard -->
         <li class="nav-item active">
@@ -20,25 +22,29 @@
         </li>
 
         <!-- Divider -->
-        <li><hr class="sidebar-divider" /></li>
+        <li>
+            <hr class="sidebar-divider" />
+        </li>
 
         <!-- Heading -->
-        <li><div class="sidebar-heading">
-            Scripts
-            </div></li>
+        <li>
+            <div class="sidebar-heading">
+                Scripts
+            </div>
+        </li>
 
         <li class="nav-item">
             <a class="nav-link" href="LitCalEngine.php">
                 <i class="fas fa-fw fa-folder"></i>
                 <span>API endpoint</span></a>
         </li>
-    
+
         <li class="nav-item">
             <a class="nav-link" href="dist/">
                 <i class="fas fa-fw fa-folder"></i>
                 <span>Swagger / Open API Docs</span></a>
         </li>
-    
+
         <li class="nav-item">
             <a class="nav-link" href="easter.php">
                 <i class="fas fa-fw fa-folder"></i>
@@ -46,13 +52,17 @@
         </li>
 
         <!-- Divider -->
-        <li><hr class="sidebar-divider" /></li>
-    
+        <li>
+            <hr class="sidebar-divider" />
+        </li>
+
         <!-- Heading -->
-        <li><div class="sidebar-heading">
-            Examples
-            </div></li>
-    
+        <li>
+            <div class="sidebar-heading">
+                Examples
+            </div>
+        </li>
+
         <li class="nav-item">
             <a class="nav-link" href="examples/php/">
                 <i class="fas fa-fw fa-folder"></i>
@@ -75,12 +85,16 @@
         </li>
 
         <!-- Divider -->
-        <li><hr class="sidebar-divider d-none d-md-block" /></li>
+        <li>
+            <hr class="sidebar-divider d-none d-md-block" />
+        </li>
 
         <!-- Sidebar Toggler (Sidebar) -->
-        <li><div class="text-center d-none d-md-inline">
-            <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div></li>
+        <li>
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+        </li>
     </ul>
     <!-- End of Sidebar -->
 
@@ -90,50 +104,39 @@
         <!-- Main Content -->
         <div id="content">
             <!-- Topbar -->
-            <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+            <nav class="preview-bar bg-light topbar mb-4 static-top shadow">
                 <!-- Topbar Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item no-arrow">
-                        <a class="nav-link" href="#" target="_self" title="English">
-                            English
-                        </a>
-                    </li>
-                    <li class="nav-item no-arrow">
-                        <a class="nav-link" href="#" target="_self" title="French">
-                            French
-                        </a>
-                    </li>
-                    <li class="nav-item no-arrow">
-                        <a class="nav-link" href="#" target="_self" title="German">
-                            German
-                        </a>
-                    </li>
-                    <li class="nav-item no-arrow">
-                        <a class="nav-link" href="#" target="_self" title="Italian">
-                            Italian
-                        </a>
-                    </li>
-                    <li class="nav-item no-arrow">
-                        <a class="nav-link" href="#" target="_self" title="Latin">
-                            Latin
-                        </a>
-                    </li>
-                    <li class="nav-item no-arrow">
-                        <a class="nav-link" href="#" target="_self" title="Portuguese">
-                            Portuguese
-                        </a>
-                    </li>
-                    <li class="nav-item no-arrow">
-                        <a class="nav-link" href="#" target="_self" title="Spanish">
-                            Spanish
-                        </a>
-                    </li>
-                    <li class="nav-item no-arrow">
-                        <a class="nav-link" href="https://github.com/JohnRDOrazio/LiturgicalCalendar" target="_blank" title="Fork me on GitHub">
-                            <i class="fab fa-github"></i>
-                        </a>
-                    </li>
-                </ul>
+                <div class="container-fluid h-100">
+                    <div class="row align-items-center justify-content-between h-100">
+                        <div class="col text-right">
+                            <a class="btn btn-transparent-dark btn-sm mr-2" href="#" target="_self" title="English">
+                                English
+                            </a>
+                            <a class="btn btn-transparent-dark btn-sm mr-2" href="#" target="_self" title="French">
+                                French
+                            </a>
+                            <a class="btn btn-transparent-dark btn-sm mr-2" href="#" target="_self" title="German">
+                                German
+                            </a>
+                            <a class="btn btn-transparent-dark btn-sm mr-2" href="#" target="_self" title="Italian">
+                                Italian
+                            </a>
+                            <a class="btn btn-transparent-dark btn-sm mr-2" href="#" target="_self" title="Latin">
+                                Latin
+                            </a>
+                            <a class="btn btn-transparent-dark btn-sm mr-2" href="#" target="_self" title="Portuguese">
+                                Portuguese
+                            </a>
+                            <a class="btn btn-transparent-dark btn-sm mr-2" href="#" target="_self" title="Spanish">
+                                Spanish
+                            </a>
+                            <a class="btn btn-transparent-dark btn-sm mr-2"
+                                href="https://github.com/JohnRDOrazio/LiturgicalCalendar" target="_blank"
+                                title="Fork me on GitHub">
+                                <i class="fab fa-github"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </nav>
-
             <div class="container-fluid">
