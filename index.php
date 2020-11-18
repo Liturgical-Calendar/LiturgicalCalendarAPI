c68be47fec4d95a4d4ff0817e9f0b795bd5b556b
John D'Orazio <priest@johnromanodorazio.com>
2020-11-18 12:08:34 +0100

    create echo translation function and add some strings

--- index.php
+++ index.php
@@ -5,15 +5,18 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $LOCALE = Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+$LOCALE = !empty($_COOKIE["currentLocale"]) ? $_COOKIE["currentLocale"] : $LOCALE;
 if($LOCALE !== null){
     //we only need the two letter ISO code, not the national extension
     if(strpos($LOCALE,"_")){
         $LOCALE = explode("_", $LOCALE)[0];
+    } else if (strpos($LOCALE,"-")){
+        $LOCALE = explode("-", $LOCALE)[0];
     }
 } else {
     $LOCALE = "en";
 }
-define("LITCAL_LOCALE", isset($_COOKIE("currentLocale")) ? $_COOKIE("currentLocale") : $LOCALE );
+define("LITCAL_LOCALE", $LOCALE );
 
 /**
  * Translation function __()
@@ -39,6 +42,29 @@ function __($key, $locale)
 }
 
 /**
+ * Translation function _e()
+ */
+
+function _e($key, $locale)
+{
+    global $messages;
+    $lcl = strtolower($locale);
+    if (isset($messages)) {
+        if (isset($messages[$key])) {
+            if (isset($messages[$key][$lcl])) {
+                echo $messages[$key][$lcl];
+            } else {
+                echo $messages[$key]["en"];
+            }
+        } else {
+            echo $key;
+        }
+    } else {
+        echo $key;
+    }
+}
+
+/**
  * Define our translation strings
 */
 $messages = [
@@ -49,24 +75,40 @@ $messages = [
         "fr" => "Calendrier Général Romain",
         "it" => "Calendario Romano Generale",
         "pt" => "Calendário Romano Geral",
+    ],
+    "Catholic Liturgical Calendar" => [
+        "de" => "Katholischer Liturgischer Kalender",
+        "en" => "Catholic Liturgical Calendar",
+        "es" => "Calendario Litúrgico Católico",
+        "fr" => "Calendrier Liturgique Catholique",
+        "it" => "Calendario Liturgico Cattolico",
+        "pt" => "Calendário Litúrgico Católico",
+    ],
+    "API_DESCRIPTION" => [
+        "de" => "Eine API für den liturgischen Kalender, aus der Sie Daten für die liturgischen Ereignisse eines bestimmten Jahres ab 1970 abrufen können, sei es für den universellen oder allgemeinen römischen Kalender oder für abgeleitete nationale und diözesane Kalender",
+        "en" => "A Liturgical Calendar API from which you can retrieve data for the Liturgical events of any given year from 1970 onwards, whether for the Universal or General Roman Calendar or for derived National and Diocesan calendars",
+        "es" => "Una API para el Calendario Litúrgico del cual puede recuperar datos para los eventos litúrgicos de cualquier año dado desde 1970 en adelante, ya sea para el Calendario Romano General o Universal o para los calendarios nacionales y diocesanos derivados.",
+        "fr" => "Une API pour le calendrier liturgique à partir de laquelle vous pouvez récupérer des données pour les événements liturgiques d'une année donnée à partir de 1970, que ce soit pour le calendrier romain universel ou général ou pour les calendriers nationaux et diocésains dérivés",
+        "it" => "Una API per il Calendario Liturgico, da cui estrarre i dati degli eventi liturgici di un qualsiasi anno dal 1970 in poi, sia per il Calendario Romano Universale che per i calendari nazionali e diocesani derivati",
+        "pt" => "Uma API para o calendário litúrgico do qual você pode recuperar dados para os eventos litúrgicos de qualquer ano a partir de 1970, seja para o calendário romano universal ou geral ou para calendários nacionais e diocesanos derivados"
     ]
 ];
 
 ?>
 
 <!doctype html>
-<html lang="en">
+<html lang="<?php echo LITCAL_LOCALE; ?>">
 <head>
-    <title><?php __("General Roman Calendar",LITCAL_LOCALE) ?></title>
-    <?php echo  file_get_contents('layout/head.php'); ?>
+    <title><?php _e("General Roman Calendar",LITCAL_LOCALE) ?></title>
+    <?php include_once('layout/head.php'); ?>
 </head>
 <body>
 
-    <?php echo  file_get_contents('layout/header.php'); ?>
+    <?php include_once('layout/header.php'); ?>
 
         <!-- Page Heading -->
-        <h1 class="h3 mb-2 text-gray-800">Catholic Liturgical Calendar</h1>
-        <p class="mb-4">An API for the Liturgical Calendar for any given year, based on the Universal or General Roman Calendar and for derived National and Diocesan calendars</p>
+        <h1 class="h3 mb-2 text-gray-800"><?php _e("Catholic Liturgical Calendar",LITCAL_LOCALE); ?></h1>
+        <p class="mb-4"><?php _e("API_DESCRIPTION",LITCAL_LOCALE) ?></p>
 
         <!-- Page Heading -->
         <h3 class="h3 mb-2 text-gray-800">SCRIPTS</h3>
@@ -148,7 +190,7 @@ $messages = [
         </div>
         <!-- /.row -->
 
-    <?php echo  file_get_contents('layout/footer.php'); ?>
+    <?php include_once('layout/footer.php'); ?>
 
 </body>
 </html>
