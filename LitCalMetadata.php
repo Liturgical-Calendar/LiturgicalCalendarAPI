f01789d767fc9688a8a7ff83fdfecc260b4c9fc8
John D'Orazio <priest@johnromanodorazio.com>
2021-12-04 00:15:20 +0100

    no need to give out paths to third parties

--- LitCalMetadata.php
+++ LitCalMetadata.php
@@ -11,7 +11,16 @@ header( 'Access-Control-Allow-Credentials: true' );
 header( 'Access-Control-Max-Age: 86400' );    // cache for 1 day
 header( 'Content-Type: application/json' );
 if( file_exists( 'nations/index.json' ) ) {
-    echo file_get_contents( 'nations/index.json' );
+    $index = file_get_contents( 'nations/index.json' );
+    if( $index !== false ) {
+        $indexAssoc = json_decode( $index, true );
+        foreach( $indexAssoc as $key => $value ) {
+            unset( $indexAssoc[$key]["path"] );
+        }
+        echo json_encode( $indexAssoc );
+    } else {
+        http_response_code(503);
+    }
 } else {
     http_response_code(404);
 }
