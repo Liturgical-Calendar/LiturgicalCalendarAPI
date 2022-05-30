4828724ff4ce90ed66e1572ccec5ede20aa21004
John D'Orazio <priest@johnromanodorazio.com>
2022-05-30 19:27:46 +0200

    implement ratchet websocket server send unit test results over the websocket

---
+++ LitCalTestServer.php
@@ -0,0 +1,18 @@
+<?php
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+    include_once('./LitCalHealth.php');
+    include_once( 'vendor/autoload.php' );
+
+    $server = IoServer::factory(
+        new HttpServer(
+            new WsServer(
+                new LitCalHealth()
+            )
+        ),
+        8080
+    );
+
+    $server->run();
