b810462a57a39fe510fcec778911e90a6c87579f
John R. D'Orazio <priest@johnromanodorazio.com>
2024-05-29 02:54:58 +0200

    debug metadata path

--- metadata/index.php
+++ metadata/index.php
@@ -1,5 +1,9 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('date.timezone', 'Europe/Vatican');
+
 include_once('../includes/enums/RomanMissal.php');
 
 use LitCal\enum\RomanMissal;
