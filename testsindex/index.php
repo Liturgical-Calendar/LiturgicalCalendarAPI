3ecbc886b887f8a199bb81e25d8b06d6cf559020
John R. D'Orazio <priest@johnromanodorazio.com>
2024-05-29 03:18:49 +0200

    fix vendor autoload path

--- testsindex/index.php
+++ testsindex/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'vendor/autoload.php';
+require_once '../vendor/autoload.php';
 require_once '../includes/enums/RequestContentType.php';
 require_once '../includes/enums/StatusCode.php';
 require_once '../includes/TestsIndex.php';
