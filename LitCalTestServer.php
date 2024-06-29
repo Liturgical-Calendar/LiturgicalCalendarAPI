4de17dfd4783a27d1fddaa3235e3fbc5ebd1cf1c
John R. D'Orazio <priest@johnromanodorazio.com>
2024-06-29 03:02:55 +0200

    define API_BASE_PATH for LitCalTestServer

--- LitCalTestServer.php
+++ LitCalTestServer.php
@@ -11,6 +11,9 @@ use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use Johnrdorazio\LitCal\Health;
 
+$apiVersion = basename(__DIR__);
+define('API_BASE_PATH', "https://litcal.johnromanodorazio.com/api/{$apiVersion}");
+
 $server = IoServer::factory(
     new HttpServer(
         new WsServer(
