3bcf60d61829e87300504c3a35d6860c61b59d48
John R. D'Orazio <priest@johnromanodorazio.com>
2024-07-03 04:55:56 +0200

    better naming for schemas

--- src/Enum/LitSchema.php
+++ src/Enum/LitSchema.php
@@ -11,13 +11,13 @@ class LitSchema
     public const PROPRIUMDESANCTIS = self::SCHEMA_BASE_PATH . "/PropriumDeSanctis.json";
     public const PROPRIUMDETEMPORE = self::SCHEMA_BASE_PATH . "/PropriumDeTempore.json";
     public const WIDERREGION       = self::SCHEMA_BASE_PATH . "/WiderRegionCalendar.json";
-    public const DECREEMEMORIALS   = self::SCHEMA_BASE_PATH . "/LitCalDecrees.json";
+    public const DECREEMEMORIALS   = self::SCHEMA_BASE_PATH . "/LitCalDecreesPath.json";
     public const I18N              = self::SCHEMA_BASE_PATH . "/LitCalTranslation.json";
     public const METADATA          = self::SCHEMA_BASE_PATH . "/LitCalMetadata.json";
     public const LITCAL            = self::SCHEMA_BASE_PATH . "/LitCal.json";
-    public const EVENTS            = self::SCHEMA_BASE_PATH . "/LitCalAllFestivities.json";
-    public const TESTS             = self::SCHEMA_BASE_PATH . "/LitCalTests.json";
-    public const MISSALS           = self::SCHEMA_BASE_PATH . "/LitCalMissals.json";
+    public const EVENTS            = self::SCHEMA_BASE_PATH . "/LitCalEventsPath.json";
+    public const TESTS             = self::SCHEMA_BASE_PATH . "/LitCalTestsPath.json";
+    public const MISSALS           = self::SCHEMA_BASE_PATH . "/LitCalMissalsPath.json";
     public const EASTER            = self::SCHEMA_BASE_PATH . "/LitCalEasterPath.json";
     public const DATA              = self::SCHEMA_BASE_PATH . "/LitCalDataPath.json";
 
