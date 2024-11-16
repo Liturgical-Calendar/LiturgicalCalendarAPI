5bd521484f3cf009cf036e9b9aa34a8b2269d381
John R. D'Orazio <priest@johnromanodorazio.com>
2024-11-16 19:27:02 +0100

    cleaner localhost detection

--- index.php
+++ index.php
@@ -18,11 +18,9 @@ ini_set('display_errors', 1);
 ini_set('date.timezone', 'Europe/Vatican');
 require_once 'vendor/autoload.php';
 
-$apiVersion = 'dev';
-if (preg_match('/^\/api\/(.*?)\/index.php$/', $_SERVER['SCRIPT_NAME'], $matches)) {
-    $apiVersion = $matches[1];
-}
-
+/**
+ * Detect server Request Scheme
+ */
 if (
     (isset($_SERVER['REQUEST_SCHEME']) && !empty($_SERVER['REQUEST_SCHEME']) && $_SERVER['REQUEST_SCHEME'] == 'https') ||
     (isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ||
@@ -33,22 +31,36 @@ if (
     $server_request_scheme = 'http';
 }
 
+/**
+ * Detect server name
+ */
 $server_name = isset($_SERVER['SERVER_NAME']) ? $_SERVER['SERVER_NAME'] : 'localhost';
-if ('localhost' === $server_name) {
+if (Router::isLocalhost()) {
     $server_name .= ':' . $_SERVER['SERVER_PORT'];
     $concurrentServiceWorkers = getenv('PHP_CLI_SERVER_WORKERS');
     if (false === $concurrentServiceWorkers || (int)$concurrentServiceWorkers < 2) {
-        die("We detected that there are not enough concurrent service workers. Perhaps set the `PHP_CLI_SERVER_WORKERS` environment variable to a value greater than 1? E.g. `PHP_CLI_SERVER_WORKERS=2 php -S $server_name`.");
+        $pre1 = '<pre style="color:red;background-color:#EFEFEF;display:inline-block;padding: 5px;">PHP_CLI_SERVER_WORKERS</pre>';
+        $pre2 = sprintf('<pre style="color:red;background-color:#EFEFEF;display:inline-block;padding:5px;">PHP_CLI_SERVER_WORKERS=2 php -S %1$s</pre>', $server_name);
+        die("Not enough concurrent service workers.<br>Perhaps set the {$pre1} environment variable to a value greater than 1? E.g. {$pre2}.");
     }
 } else {
+    $apiVersion = 'dev';
+    if (preg_match('/^\/api\/(.*?)\/index.php$/', $_SERVER['SCRIPT_NAME'], $matches)) {
+        $apiVersion = $matches[1];
+    }
     $server_name = "{$_SERVER['SERVER_NAME']}/api/{$apiVersion}";
 }
 
-// !!IMPORTANT!! There are classes that depend on this! Do NOT remove
-// Perhaps we could find a better way to set this in a class such as Core ...
+/**
+ * Define the API_BASE_PATH constant based on the server request scheme and the server name
+ * !!IMPORTANT!! There are classes that depend on this !!DO NOT REMOVE!!
+ * Perhaps we could find a better way to set this in a class such as Core ...
+ */
 define('API_BASE_PATH', "{$server_request_scheme}://{$server_name}");
 
-Router::setAllowedOrigins('allowedOrigins.php');
-Router::route();
+if (false === Router::isLocalhost()) {
+    Router::setAllowedOrigins('allowedOrigins.php');
+}
 
+Router::route();
 die();
