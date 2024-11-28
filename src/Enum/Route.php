3d9f9b3b00ec0ae00bc7c95aef3209d3008c58fe
John R. D'Orazio <priest@johnromanodorazio.com>
2024-11-28 17:42:08 +0100

    add subroutes to route class

--- src/Enum/Route.php
+++ src/Enum/Route.php
@@ -4,12 +4,18 @@ namespace LiturgicalCalendar\Api\Enum;
 
 enum Route: string
 {
-    case CALENDARS  = '/calendars';
-    case CALENDAR   = '/calendar';
-    case TESTS      = '/tests';
-    case EVENTS     = '/events';
-    case DATA       = '/data';
-    case EASTER     = '/easter';
-    case SCHEMAS    = '/schemas';
-    case MISSALS    = '/missals';
+    case CALENDARS         = '/calendars';
+    case CALENDAR          = '/calendar';
+    case CALENDAR_NATIONAL = '/calendar/nation';
+    case CALENDAR_DIOCESAN = '/calendar/diocese';
+    case DECREES           = '/decrees';
+    case TESTS             = '/tests';
+    case EVENTS            = '/events';
+    case DATA              = '/data';
+    case DATA_WIDERREGION  = '/data/widerregion';
+    case DATA_NATIONAL     = '/data/nation';
+    case DATA_DIOCESAN     = '/data/diocese';
+    case EASTER            = '/easter';
+    case SCHEMAS           = '/schemas';
+    case MISSALS           = '/missals';
 }
