ee2aac3e09200d06eb823ea5baa743349d278bea
John R. D'Orazio <priest@johnromanodorazio.com>
2025-01-03 22:22:45 +0100

    update API_VERSION to 4.0

--- src/Paths/Calendar.php
+++ src/Paths/Calendar.php
@@ -38,7 +38,7 @@ use LiturgicalCalendar\Api\Params\CalendarParams;
  */
 class Calendar
 {
-    public const API_VERSION                        = '3.9';
+    public const API_VERSION                        = '4.0';
     public static Core $Core;
 
     private string $CacheDuration                   = "";
